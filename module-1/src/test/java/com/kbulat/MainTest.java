1fcf8f7608451542a834a7ab136f5d710587d7ba
Kyrylo Bulat <kyrylo.bulat@gmail.com>
2024-01-19 21:44:38 +0100

    Multi module Maven project

---
+++ module-1/src/test/java/com/kbulat/MainTest.java
@@ -0,0 +1,13 @@
+package com.kbulat;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+  @Test
+  void test() {
+    int a = 1;
+    Assertions.assertEquals(1, a);
+  }
+}
---
+++ module-2/src/main/java/com/kbulat/Main.java
@@ -0,0 +1,7 @@
+package com.kbulat;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
